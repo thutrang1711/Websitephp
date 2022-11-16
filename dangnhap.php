e0f12cfde04edcf853bd61d9a6a405f2af13c0ea
ngocthun26 <114061233+ngocthun26@users.noreply.github.com>
2022-11-16 21:44:14 +0700

    Add files via upload

---
+++ dangnhap.php
@@ -0,0 +1,24 @@
+    <HTML>
+    <meta charset="utf8">
+	    <HEAD>
+		    <TITLE> Đăng nhập hệ thống </TITLE>
+		    <link rel="stylesheet" type="text/css" href="style.css"/>
+	    </HEAD>
+
+	    <BODY>
+	    <section style="padding-top: 100px;height:65vh">
+        <div class="container">
+            <div class="box" >
+                <form action="" class="box_form" >
+                    <label for="name">Tên tài khoản</label>
+                    <input type="text" placeholder="tên đăng nhập" required class="txt-intput">
+                    <label for="name">Mật khẩu</label>
+                    <input type="text" placeholder="password" required class="txt-intput">
+                    <button class="btn">Đăng nhập</button>
+                </form>
+                Chưa có tài khoản<a href="dangky.php?do=register/"> đăng ký</a>
+            </div>
+        </div>
+    </section>
+    </body>
+    </HTML>
